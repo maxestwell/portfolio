0f0d3393542e9c7a8a86fe98ff512f13fc6a44ed
maxestwell <maxwellpcorcoran@gmail.com>
2024-11-06 13:07:43 +1100

    build

--- src/router/index.js
+++ src/router/index.js
@@ -14,12 +14,6 @@ const router = createRouter({
       component: () => import('../views/Project.vue'),
       props: true,
     },
-    {
-      path: '/project/:projectId/tasks',
-      name: 'taskLists',
-      component: () => import('../views/TaskLists.vue'),
-      props: true,
-    },
   ],
 })
 
