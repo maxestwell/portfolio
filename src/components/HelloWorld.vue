cf1e0e9cc921639e1cee8105b75b31f5e0226a26
maxestwell <maxwellpcorcoran@gmail.com>
2024-11-07 12:31:56 +1100

    cool

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,30 +2,55 @@
 defineProps<{
   msg: string
 }>()
+import { ref, onMounted } from 'vue'
+import { gsap } from 'gsap'
 import { RouterLink, RouterView } from 'vue-router'
+
+const textElement = ref<HTMLElement | null>(null)
+
+onMounted(() => {
+  const handleMouseEnter = () => {
+    const isElement = textElement.value?.querySelector('i')
+    if (isElement) {
+      gsap.to(isElement, {
+        duration: 0.5,
+        fontVariationSettings: "'slnt' -10, 'wght' 900",
+      })
+    }
+  }
+
+  const handleMouseLeave = () => {
+    const isElement = textElement.value?.querySelector('i')
+    if (isElement) {
+      gsap.to(isElement, {
+        duration: 0.5,
+        fontVariationSettings: "'slnt' 0, 'wght' 400",
+      })
+    }
+  }
+
+  textElement.value?.addEventListener('mouseenter', handleMouseEnter)
+  textElement.value?.addEventListener('mouseleave', handleMouseLeave)
+})
 </script>
 
 <template>
   <div class="maxiswell">
-    <h1 class="heading">
+    <h1 class="heading" ref="textElement">
       <RouterLink to="/">max <i>is</i> well</RouterLink>
     </h1>
   </div>
 </template>
 
 <style scoped>
-a {
-  /* height: 100%; */
-  font-weight: 700;
-}
-
 i {
-  font-style: italic;
-  font-weight: 700;
+  font-style: normal;
+  font-variation-settings:
+    'slnt' 0,
+    'wght' 400;
 }
 
-/* .maxiswell {
-  display: flex;
-  align-items: baseline;
-} */
+.heading {
+  display: inline-block;
+}
 </style>
