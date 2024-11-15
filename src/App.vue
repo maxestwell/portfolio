1aaef9f37e8bb4de0087f12e7b6bd9a68b4779bf
maxestwell <maxwellpcorcoran@gmail.com>
2024-11-15 13:54:24 +1100

    deploy redirect

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,8 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
-
     <div class="wrapper">
       <HelloWorld msg="max is well" />
-
-      <nav>
-        <!-- <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink> -->
-      </nav>
     </div>
   </header>
 
