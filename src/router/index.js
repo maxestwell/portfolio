639d5408f63893e239431de358cac91283093a3a
maxestwell <maxwellpcorcoran@gmail.com>
2024-11-16 12:04:38 +1100

    change router

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(), // Use history mode for clean URLs
   routes: [
     {
       path: '/',
