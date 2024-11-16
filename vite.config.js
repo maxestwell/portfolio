9396af07eee708778e3ab58ca07e43155d180f72
maxestwell <maxwellpcorcoran@gmail.com>
2024-11-16 12:38:06 +1100

    moved

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/vue-project/',
+  base: '/',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
