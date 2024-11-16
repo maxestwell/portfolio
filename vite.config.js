a00a90129a624f4b278e91d761480aa5c69e0be7
maxestwell <maxwellpcorcoran@gmail.com>
2024-11-16 12:57:49 +1100

    update name

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/portfolio/',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
